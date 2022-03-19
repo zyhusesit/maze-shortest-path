3ca96f546a850a5b91b43e2bf5324efc882fd23f
Sam331 <36762547+zyhusesit@users.noreply.github.com>
2022-03-20 00:50:59 +0800

    I don't have idea!

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 struct Point {
-    int x, y;
-    bool status = false;
+    int x{}, y{};
 };
 
 struct Path {
     Point *node_point = nullptr;
     Path *next = nullptr;
 };
+
 
 bool checkPath(Path *test_path);
 
@@ -20,9 +20,13 @@ void *pushStack(Path *path, Point *new_point);
 
 Point *popStack(Path *path);
 
+bool isEmpty(Path *path);
+
 void displayPath(Path *path);
 
 void moveForward(Path *path, int operator_num);
+
+void searchForPath(Path *path, Point *start_point, Point *end_point);
 
 int maze[10][10] = {
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -38,6 +42,10 @@ int maze[10][10] = {
 
 int main() {
     int start_x, start_y, end_x, end_y;
+    auto *start_point = new Point;
+    auto *end_point = new Point;
+    Path *test_path = new Path;
+
     cout << "Please enter the X direction of your start point:" << endl;
     cin >> start_x;
     cout << "Please enter the Y direction of your start point:" << endl;
@@ -47,17 +55,27 @@ int main() {
     cout << "Please enter the Y direction of your end point:" << endl;
     cin >> end_y;
 
+
+    start_point->x = start_x;
+    start_point->y = start_y;
+    end_point->x = end_x;
+    end_point->y = end_y;
+
+    searchForPath(test_path, start_point, end_point);
+
 }
 
 bool checkPath(Path *test_path) {
-    if (!getTop(test_path)->status)
+    Point *point = getTop(test_path);
+    if (maze[point->y][point->x] == 0)
         return false;
     else
         return true;
+
 }
 
 Point *getTop(Path *path) {
-    if (path->next == nullptr) {
+    if (isEmpty(path)) {
         cout << "ERROR! The path stack is empty." << endl;
     } else
         return path->next->node_point;
@@ -71,11 +89,11 @@ void *pushStack(Path *path, Point *new_point) {
 }
 
 Point *popStack(Path *path) {
-    if (path->next == nullptr) {
+    if (isEmpty(path)) {
         cout << "ERROR! The path stack is empty." << endl;
     } else {
         Path *ptr = path->next;
-        auto *pop_point = new Point;
+        Point *pop_point;
         pop_point = ptr->node_point;
         path->next = ptr->next;
         delete ptr;
@@ -83,16 +101,28 @@ Point *popStack(Path *path) {
     }
 }
 
+bool isEmpty(Path *path) {
+    if (path->next == nullptr)
+        return true;
+    else
+        return false;
+}
+
+
 void displayPath(Path *path) {
-    Path *ptr = path->next;
-    while (ptr) {
-        cout << "(" << ptr->node_point->x << "," << ptr->node_point->y << ")<--";
-        ptr = ptr + 1;
+    if (isEmpty(path))
+        cout << "ERROR! The path stack is empty." << endl;
+    else {
+        Path *ptr = path->next;
+        while (ptr != nullptr) {
+            cout << "(" << ptr->node_point->x << "," << ptr->node_point->y << ")<--";
+            ptr = ptr->next;
+        }
     }
 }
 
 void moveForward(Path *path, int operator_num) {
-    Point *new_point = new Point;
+    auto *new_point = new Point;
     switch (operator_num) {
         case 1://up
             new_point->y = getTop(path)->y - 1;
@@ -102,11 +132,11 @@ void moveForward(Path *path, int operator_num) {
             new_point->y = getTop(path)->y;
             new_point->x = getTop(path)->x + 1;
             break;
-        case 3:
+        case 3://down
             new_point->y = getTop(path)->y + 1;
             new_point->x = getTop(path)->x;
             break;
-        case 4:
+        case 4://left
             new_point->y = getTop(path)->y;
             new_point->x = getTop(path)->x - 1;
             break;
@@ -114,4 +144,44 @@ void moveForward(Path *path, int operator_num) {
             break;
     }
     pushStack(path, new_point);
+}
+
+void searchForPath(Path *path, Point *start_point, Point *end_point) {
+    pushStack(path, start_point);
+    int i = 1;
+    while (getTop(path)->x != end_point->x && getTop(path)->y != end_point->y) {
+
+        moveForward(path, 1);
+        if (!checkPath(path)) {
+            i = 1;
+            continue;
+        } else {
+            popStack(path);
+            i++;
+        }
+        moveForward(path, 2);
+        if (!checkPath(path)) {
+            i = 1;
+            continue;
+        } else {
+            popStack(path);
+            i++;
+        }
+        moveForward(path, 3);
+        if (!checkPath(path)) {
+            i = 1;
+            continue;
+        } else {
+            popStack(path);
+            i++;
+        }
+        moveForward(path, 4);
+        if (!checkPath(path)) {
+            i = 1;
+            continue;
+        } else {
+            break;
+        }
+    }
+    displayPath(path);
 }
